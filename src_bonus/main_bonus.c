832f28c60a02ef77d910e48e67b644640c24c038
Elliot Bouvier <ebouvier@student.42.fr>
2023-06-28 11:18:21 +0200

    append instead of trunc

--- src_bonus/main_bonus.c
+++ src_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 11:41:37 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/06/24 13:45:28 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/06/28 11:17:50 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,15 @@ int	parse_av(int ac, char **av, char **env, t_pipe *p)
 	if (check_here_doc(p->av[1]))
 		p->here_doc = 1;
 	if (p->here_doc)
+	{
 		handle_here_doc(p);
+		p->fd_out = open(p->av[p->ac - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+	}
 	else
+	{
 		p->fd_in = open(p->av[1], O_RDONLY);
-	p->fd_out = open(p->av[p->ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		p->fd_out = open(p->av[p->ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	}
 	if (p->fd_in < 0 || p->fd_out < 0)
 	{
 		close_if_open(p->fd_in);
